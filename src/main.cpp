a07bd97a994fbb63cecf7829b0fea44ca8b32f08
joosh3 <joosh3@joosh3-ubuntu.(none)>
2014-01-02 03:14:17 +0100

    more changes

--- src/main.cpp
+++ src/main.cpp
@@ -1983,7 +1983,7 @@ bool LoadBlockIndex(bool fAllowNew)
         pchMessageStart[1] = 0xc0;
         pchMessageStart[2] = 0xb8;
         pchMessageStart[3] = 0xdb;
-        hashGenesisBlock = uint256("0x5813d4cfab0eeda94a15d11c5e7d6895e667fbbe67c59ef9a1b3bc232c9a0b7f");
+        hashGenesisBlock = uint256("0xe96e264b6381c875b7b5701146ee2b4c35e7d23ab2c66b5dfc2ad601bf992599");
     }
 
     //
@@ -2023,17 +2023,17 @@ bool LoadBlockIndex(bool fAllowNew)
         if (fTestNet)
         {
             block.nTime    = 1388625571;
-            block.nNonce   = 440824;
+            block.nNonce   = 644305;
         }
 
         //// debug print
         printf("%s\n", block.GetHash().ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x2fc1b7ef46270c053711fbae934cf7f83378efd4b3e158079451d9c6c90e4700"));
+        assert(block.hashMerkleRoot == uint256("0xc711fa831e78a01820e09aa211cf9507360e8fbcc36d69243e55af74d1f1d0fb"));
 
         // If genesis block hash does not match, then generate new genesis hash.
-        if (false && block.GetHash() != hashGenesisBlock)
+        if (true && block.GetHash() != hashGenesisBlock)
         {
             printf("Searching for genesis block...\n");
             // This will figure out a valid hash and Nonce if you're
